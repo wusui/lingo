f86c3d90c15af8618dfcc6f12789bc6503bfdb75
Warren Usui <wusui@redhat.com>
2020-07-03 01:03:14 -0700

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,26 @@
+/* Copyright (c) 2020 Warren Usui, MIT License */
+/*jslint browser:true */
+
+/**
+ * localStorage objects
+ */
+var WORD_COUNT = "word_count";
+var SHORT_LIST = "short_list";
+var LONG_LIST = "long_list";
+var GUESSES = "guesses";
+var GUESS_PAT = "guess_pat";
+
+/**
+ * Canvas values
+ */
+var RED = "#FF0000";
+var YELLOW = "#FFFF00";
+var BLACK = "#000000";
+var FONT = "60px Arial";
+
+/**
+ * Html objects
+ */
+var GUESS = "guess";
+var MYCANVAS = "myCanvas1";
+var BBUTTON = "bbutton";
